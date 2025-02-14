b7dd9fed10f47daeeb7831c0f5580ccb548501c8
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-14 12:42:56 +0000

    conversions and Basic tricks

---
+++ 03_Conversion.js
@@ -0,0 +1,56 @@
+/*let score = "33"
+console.log(typeof score)
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+*/
+
+/*
+Number conversion:
+if there are numbers in a string then it can be converted easily in numbers
+but if string like "33asd" converted in number but the value will be NaN {not a number}
+boolean converted in 0 or 1
+undefined converted into NaN
+and Null converted into 0
+here a imp point the value will be NaN but type is Number 
+
+
+Boolean Conversion
+0 => false; 1 =>true
+"" => false; "sachch" => true;
+*/
+
+/*
+let age = 33
+console.log(age);
+console.log(typeof age);
+
+let strAge = String(age)
+console.log(strAge);
+console.log(typeof strAge);
+*/
+
+//****************Oprations*****************
+
+//let value = 9
+//let negValue = -value
+//console.log(negValue);
+//console.log(typeof negValu
+
+//console.log(2**4);//power
+//console.log(6/3);//devide
+//console.log(8%3);//remainder
+
+//console.log(((2 + 3) * 5)/5); //tricky code
+
+/*
+//prefix increment
+let x = 3
+const y = ++x
+console.log(x,y);//4 4
+
+//postfix increment
+let a = 5
+const b = a++
+console.log(a , b);//6 5
+*/
