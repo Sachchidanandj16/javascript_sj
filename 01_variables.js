4b8e9fb6cb9a0a863cf7147665af2ba0f4f41f8b
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-14 09:34:15 +0000

    commit changes done

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const user_Id = 2115000881 //constant 
+let acEmail = "sachchidanandjh1607@gmail.com"
+var pass = "9319803343"
+city = "Mathura" //var can be written as this but prefer not to use
+
+acEmail = "Sachchu@234gmail"
+console.log(user_Id);
+console.log(acEmail);
+console.log(pass);
+console.log(city);
+console.table([user_Id, acEmail, pass, city]) //print multiple in tabular form
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
