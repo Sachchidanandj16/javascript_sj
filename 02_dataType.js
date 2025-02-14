a0a983419353bffedb8ae56b881cb4c79282d060
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-14 10:18:05 +0000

    commited

---
+++ 02_dataType.js
@@ -0,0 +1,21 @@
+let name = "sachchidanandjha"  //string
+let age = 21 //int
+let isLoggedIn = true //boolean
+
+// number = 2 to the pow 53 range
+//bigInt
+//String = " "
+// boolean = true/ false
+// null = standalone value
+//undefined
+// Symbol = unique
+
+//Object 
+
+console.log(typeof "sachchidanandjha")
+console.log(typeof null) //Object
+console.log(typeof undefined) //Undefined
+
+//null is a Object but undefined is undefined
+
+let demo = null // we
