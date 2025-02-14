749e4a387dcee41ae5c6b1294d14b39122a9aca0
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-14 20:04:41 +0000

    diff. types comparisons in JS

---
+++ 04_comparison.js
@@ -0,0 +1,20 @@
+//console.log(2>4)
+//console.log(2<4)
+//console.log(3<=3)
+//console.log(4!=6)
+//console.log(5==5)
+
+//console.log("5">2)
+//console.log(5<"2")
+//console.log("Ram">45);
+
+
+//console.log(null<3);
+//console.log(null>3);
+//console.log(null>=5);
+
+//console.log(undefined>0);
+//console.log(undefined<0);
+//console.log(undefined==0);
+
+console.log("2" === 2);
