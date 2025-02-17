ef23017ce6872fa5cedf5a173929940eda4b8ca1
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-17 18:14:26 +0000

    Data Types and  syntax in JS

---
+++ 05_dataTypes_Sum.js
@@ -0,0 +1,28 @@
+/*
+1. Primitive Data Types
+Type of Primitive dataTypes: String, Number, Boolean, BigInt, Symbol, Null, Undefined
+
+2.Refrence(Non-Primitive DataTypes)
+Array, Objects, Functions
+
+
+=>JavaScript is a dynamic programming language. This means that it allows dynamic typing
+(variables can hold different types of data at different points in time)
+Example:
+let a = 5; // number
+a = "Hello"; // now it's a string
+
+//Object
+let myObj = {name: "Sachch", age:34} //creating Object
+console.log(typeof myObj);
+console.log(myObj);
+
+const human = ["Rajendra", "Diwakar", "Devendra", "Kaushal", "Satendra"] //Creating array
+console.log(human, typeof human);
+
+const myfunct = function(){ //creating function
+    console.log("Hello World");
+    
+}
+myfunct();//calling function
+*/
