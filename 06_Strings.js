948a2f42e3e6dee012210d6ec276591b95d4e72a
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-19 21:55:01 +0000

    Strings In JS

---
+++ 06_Strings.js
@@ -0,0 +1,38 @@
+/*
+let user = "Sachchidanand Jha "
+let age = 20
+console.log("Here the data "+user+ age)
+console.log(`hello my name is ${user}and my age is ${age}.`);
+*/
+//const gameName = new String('Sachchidanand jha')
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(14));
+//console.log(gameName.indexOf('a'));
+
+//const gameName = new String('Sachchidanand Jha')
+//console.log(gameName);
+//console.log(typeof gameName);
+
+//SubString function
+//const newGameName = gameName.substring(0/*StartNumber*/,14/*EndNumber*/)
+//console.log(newGameName);
+//console.log(typeof newGameName)
+
+//const onemoreString = gameName.slice(-35,8)
+//console.log(onemoreString)
+
+//const word = '   Hare Krishna   '
+//console.log(word);
+//console.log(word.trim());//Trim Removes Extra Spaces in String
+
+//const url = "https://m.youtube%54% tutorial.com"
+//console.log(url.replace('%54%',''))
+//console.log(url.replace('%54%','><'))
+
+//console.log(url.includes('you'))//returns boolean use for asking is present or not
+//console.log(url.split('%'));//this funct take inside the value on which string can devide in array
+//let amar  = url.split('%')
+//console.log(typeof amar)
+//
+//console.log(`Hello EveryOne my name is ${"Sachchidanand"} and i am from ${"Vrindavan"}, Here i am proudly presenting you a PPT on ${"MyLIFE_MYRULES"}`)
