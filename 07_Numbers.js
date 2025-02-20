abce49fd39f32a24350248bb9b3e9b8402b55784
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-20 17:29:55 +0000

    Numbers in jS

---
+++ 07_Numbers.js
@@ -0,0 +1,53 @@
+//const Binam = new Number(100) //output: [Number: 100]
+//const name = 100 //output: 100
+//console.log(Binam);
+//console.log(typeof Binam)
+//
+//console.log(Binam.toString())
+//console.log(typeof(Binam.toString()))
+//
+//console.log("Using Name "+name);
+
+//const balance = new Number(100)
+//console.log(typeof balance)
+//const manat = balance.toString()//converts into strings
+//console.log(typeof manat)
+//console.log(balance.toFixed(2))//100.00
+
+//const roundvalue = 735.519
+//console.log(roundvalue.toPrecision(4))
+
+//const undefNum = 199098765
+//console.log(undefNum.toLocaleString('en-IN'))//set the large values
+
+// let basis = 100
+// console.log(basis.ma);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++\\
+
+// console.log("Using math.abs on 5.2=> "+Math.abs(-5))
+// console.log("Using Math.round on 5.2=> "+Math.round(5.2))
+// console.log("Using Math.ceil on 5.2=> "+Math.ceil(5.2))//ceil returnes +1 value if any digit after decimal
+// console.log("Using Math.floor on 5.2=> "+Math.floor(5.2))//floor returns same value dont care whats after decimal
+
+// console.log(Math.sqrt(5));//finds square Root
+
+// console.log(Math.random())//giving random values b/w 0 and 1
+// console.log((Math.random()*10))//now b/w 0 and 10
+// console.log((Math.random()*10)+1)//now min 1 and max 10
+
+
+// console.log(Math.floor(Math.random()*10)+1)//using floor for taking single digit output
+// console.log(Math.ceil(Math.random()*10)+1)// using floor for taking single digit output
+
+// const max = 15
+// const min = 10
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
+/* 
+In this Math.floor() making output in single digit,and Math.random() gives random no.
+because this function returns no. b/w 0 and 1 so multiply it my diffrence of given two limits
+and to stop from being zero adding one and in the last we add min value for future outputs.
+*/
+
+
