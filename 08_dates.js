e45441ab16220b65592a95bf5b6a4f180501f82f
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-02-20 19:29:13 +0000

    date and time in JS

---
+++ 08_dates.js
@@ -0,0 +1,29 @@
+// let myDate = new Date()
+// console.log(myDate.toString()) //Thu Feb 20 2025 18:29:27 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toLocaleString()); // 2/20/2025, 6:29:27 PM
+// console.log(myDate.toDateString()); // Thu Feb 20 2025
+// console.log(myDate.toISOString()); // 2025-02-20T18:29:27.630Z
+
+// let ownDate = new Date(2004, 6, 16)
+// console.log(ownDate.toDateString()) //Fri Jul 16 2004
+
+// let ownDate = new Date(2004, 6, 16, 16,50)
+// console.log(ownDate.toLocaleString()) // 7/16/2004, 4:50:00 PM
+
+// let ownDate = new Date("07-16-2004")
+// console.log(ownDate.toDateString())
+
+// +++++++++++++++++++++ TimeStamp +++++++++++++++++++++//
+
+// let NowTimeStamp = Date.now()
+// console.log(NowTimeStamp)
+// console.log(ownDate.getTime())
+
+// console.log(Math.floor(Date.now()/1000))// converts in seconds.
+
+// let newdate = new Date()
+// console.log(newdate)
+// console.log(newdate.getDay()+1) // starts from 0 so add 1 for not cofusing
+// console.log(newdate.getMonth()+1) // starts from 0 so add 1 for not cofusing
+
+// console.log(`Hey i am sachchidanand jha and today date is ${Date(Date.toString())}`)
