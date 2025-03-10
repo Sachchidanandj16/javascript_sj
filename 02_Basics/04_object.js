47a0ec6a875133710e45ffed880f6503ec5fa540
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-03-10 16:11:05 +0000

    Destructuring of Object

---
+++ 02_Basics/04_object.js
@@ -0,0 +1,91 @@
+// const tinderuser = new Object()
+
+// const tinderuser = {} //alternate way to initiate.
+// tinderuser.id = "5673kjhsg"
+// tinderuser.name = "sachchidanand"
+// tinderuser.isLoggedin = true
+
+// console.log(tinderuser)
+
+// const regularUser = {
+    // id : "2115000991",
+    // isLoggedin :false,
+    // name: {
+        // fullName:{
+            // firstname: "Sachchidanand",
+            // lastName: "Jha",
+        //   }
+    // }
+// }
+// console.log(regularUser.name?.fullName)//we can access by .
+//here ? is used for adding if else
+
+// const ab1= {1:"Ram", 2:"shyam"}
+// const ab2 = {3:"Kanha", 4:"Shyam"}
+
+// const obj00 = {ab1, ab2}
+// console.log(obj00);
+
+// const obj01 = Object.assign({}, ab1, ab2)
+// console.log(obj01)
+
+// const obj02 = {...ab1, ...ab2} //spread operator
+// console.log(obj02)
+
+// const users = [
+    // {
+        // id: 1,
+        // email: "shyama@google.com"
+    // },
+    // {
+        // id: 1,
+        // email: "shyama@google.com"
+    // },
+    // {
+        // id: 1,
+        // email: "shyama@google.com"
+    // }
+// ]
+// 
+// console.log(users[2].id) //syntax of access elemnts of object in array
+
+
+// console.log(tinderuser)
+// console.log(Object.keys(tinderuser)) //this gives output in arrays
+// console.log(Object.values(tinderuser)) //this gives output in arrays
+// console.log(Object.entries(tinderuser)) //this gives output in arrays
+
+// console.log(tinderuser.hasOwnProperty('isLoggedin'))
+
+
+//De-Structuring
+
+//const course = {
+//    courseName: "js in Hindi",
+//    price: "653",
+//    duretion: "54 min",
+//    courseInstructor: "sachchidanand"
+//}
+// console.log(course.courseName())
+// course.courseInstructor
+
+// const {duretion: baba} = course //if element of object is too hard to or long we can make it like her as baba creates
+// console.log(baba) //54 min
+
+//const {price} = course
+//const {courseInstructor: papa} = course
+
+//console.log(price);
+//console.log(papa)
+
+// JSON
+// {
+    // "name"="Sachchidanand",
+    // "courseName"="JS playlist",
+    // "price"= "free"
+// }
+{
+    {}
+    {}
+    {}
+}
