d4bb93009f11ad563190c18505c36315452100fe
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-03-19 18:40:47 +0000

    Break & continue in for Loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,54 @@
+// For
+/*
+for (let i = 0; i < 9; i++) {
+    const element = i;
+    if (element == 4) {
+        console.log(`4 is Best Number`)
+    }
+    console.log(element);
+}
+*/
+
+/*
+for(let i=1; i<=10; i++){
+    console.log(`table of ${i}`)
+    for(let j=1; j<=10;j++){
+        console.log(`${i} X ${j} = ${i*j}`)
+    }
+    console.log( )
+}
+*/
+
+/*
+let MyArray = ["SpiderMan", "flash", "IronMan", "Hulk"]
+for (let index = 0; index <= MyArray.length; index++) {
+    const element = MyArray[index];
+    console.log(element)
+}
+*/
+
+//Break and continue:-
+
+//break
+/*
+for(let i=1;i<=20;i++){
+    const element = i;
+    if(element == 10){
+        console.log(`10 is detected !!`);
+        break      
+    }
+    console.log(`Value of i is ${i}`);
+}
+*/
+
+//continue
+
+// for(let i=1;i<=10;i++){
+//     const element = i;
+//     if(element == 9){
+//         console.log(`9 is detected !!`);
+//         continue      
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
+  
