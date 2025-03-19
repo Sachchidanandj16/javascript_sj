3cca5b92b3a3d39ed792498f3e302726494cf5ee
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-03-19 18:42:44 +0000

    For In in JS

---
+++ 05_iterations/four.js
@@ -0,0 +1,22 @@
+
+// const MyObj = {
+//     js: 'javaScript',
+//     cpp:'C++',
+//     rb:'ruby',
+//     swift: 'Swift by apple'
+// }
+
+//for In Loop
+/*
+for (const key in MyObj) {
+    console.log(`${key} shortForm is for ${MyObj[key]}`);
+}
+*/
+
+// ForIn Loop on arrays
+
+// const proGramLang = ["jav", "C", "Python", "HTML", "CSS", "Node.js", "JS"]
+// for (const key in proGramLang) {
+//     console.log(proGramLang[key]);
+// }
+
