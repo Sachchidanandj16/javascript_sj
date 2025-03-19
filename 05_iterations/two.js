9628bdef231c5109e407a80ae64ec9ee839ba620
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-03-19 18:41:24 +0000

    While Loop in JS

---
+++ 05_iterations/two.js
@@ -0,0 +1,38 @@
+//While Loop
+
+/*
+let i = 1;
+while(i<=5{
+    console.log("Amitabh Bachchan");
+    i++
+}
+*/
+
+/*
+let Superheroes = ["IronMan", "Thor", "Captain America", "Bahubali"]
+let j=0;
+while(j<Superheroes.length){
+    console.log(`Current value is ${j} and value is ${Superheroes[j]}`);
+    j++
+}
+*/
+
+//Do While Loop
+
+/*
+let score= 1
+do {
+    console.log(`Score is ${score}`)
+    score = score+1;
+    
+} while (score<4);
+*/
+
+/*
+let point= 5 // here point value is 5 which is greater than condition but in do while first execute than checkes condition
+do {
+    console.log(`point is ${point}`)
+    point = point+1;
+
+} while (point<4);
+*/
