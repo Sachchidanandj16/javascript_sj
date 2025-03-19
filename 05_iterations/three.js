ef007ef79234b61a3294bc34ec960504ebe76517
Sachchidanand Jha <158037494+Sachchidanandj16@users.noreply.github.com>
2025-03-19 18:42:17 +0000

    For Of and Map in JS

---
+++ 05_iterations/three.js
@@ -0,0 +1,46 @@
+// For of
+
+/*
+const arr = [1, 2, 3, 4, 5]
+for (const i of arr) {
+    console.log(i);
+}
+*/
+
+/*
+const greet = "Hello Shastriji"
+for(const k of greet){
+    console.log(`${k} is from ${greet}'s ${k}`)
+}
+*/
+
+//Maps
+
+// const ji = new Map()
+// ji.set('IN', "India")
+// ji.set('USA', "United States of America")
+// ji.set('Fr', "France")
+
+// console.log(ji)
+
+/*
+for(const k of ji){
+    console.log(k)
+}
+*/
+
+/*
+//Break down Map values:-
+
+for(const [k, v]of ji){
+    console.log(k, ':-', v);
+}
+*/
+
+// const myObj = {
+//     'game1':'NFS',
+//     'game2':'Spiderman'
+// }
+// for(const [key, val] of myObj){
+//     console.log(key, ":-", val)
+// }
